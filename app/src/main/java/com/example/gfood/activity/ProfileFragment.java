dca2c18791e6869c6d6f36922ef280cc70ae3c61
NamNguyen1606 <53823983+NamNguyen1606@users.noreply.github.com>
2019-10-27 17:26:27 +0700

    update 27/10/2019 pick image from gallery (DONE) convert bipmap to base64 (DONE)

--- app/src/main/java/com/example/gfood/activity/ProfileFragment.java
+++ app/src/main/java/com/example/gfood/activity/ProfileFragment.java
@@ -1,18 +1,23 @@
 package com.example.gfood.activity;
 
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,6 +26,12 @@ import com.example.gfood.retrofit2.model.UserInfo;
 import com.example.gfood.retrofit2.service.APIService;
 import com.example.gfood.retrofit2.service.APIutils;
 import com.squareup.picasso.Picasso;
+import com.vansuita.pickimage.bean.PickResult;
+import com.vansuita.pickimage.bundle.PickSetup;
+import com.vansuita.pickimage.dialog.PickImageDialog;
+import com.vansuita.pickimage.listeners.IPickResult;
+
+import java.io.ByteArrayOutputStream;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,8 +45,10 @@ public class ProfileFragment extends Fragment {
 
     private ImageView imgAvatarUser;
     private TextView tvUsername, tvAddress, tvPhoneNumber, tvChangePwd, tvPayment, tvHistory, tvSignOut;
+    private Button btnEdit;
     private SharedPreferences sharedPreferences;
     private APIService apiService;
+    private Bitmap avatar;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -54,6 +67,7 @@ public class ProfileFragment extends Fragment {
         tvPayment = (TextView) view.findViewById(R.id.tvProfPayment);
         tvHistory = (TextView) view.findViewById(R.id.tvProfHistory);
         tvSignOut = (TextView) view.findViewById(R.id.tvProfSignOut);
+        btnEdit = (Button) view.findViewById(R.id.btnProfEdit);
 
         Context context = getActivity();
         sharedPreferences = context.getSharedPreferences("Acount_info", Context.MODE_PRIVATE);
@@ -124,6 +138,25 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //Change avatar
+        imgAvatarUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PickImageDialog.build(new PickSetup(), new IPickResult() {
+                    @Override
+                    public void onPickResult(PickResult r) {
+                        avatar = r.getBitmap();
+                        Picasso.with(getContext()).load(r.getUri()).resize(250, 200).into(imgAvatarUser);
+                        String avatarBase64 = encodeImage(avatar);
+                        Log.e("BASE64", avatarBase64);
+                    }
+                }).show(getActivity().getSupportFragmentManager());
+
+
+            }
+        });
+
         //Change Password
         tvChangePwd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -144,6 +177,51 @@ public class ProfileFragment extends Fragment {
                 getActivity().finish();
             }
         });
+
+        //Edit profile
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogEdit();
+            }
+        });
+    }
+
+
+    private void dialogEdit(){
+        Context context;
+        final Dialog dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.dialog_edit_profile);
+
+        EditText tvName = (EditText) dialog.findViewById(R.id.edtProfEditName);
+        EditText tvPhoneNumber = (EditText) dialog.findViewById(R.id.edtProfEditPhoneNumber);
+        EditText tvAddress = (EditText) dialog.findViewById(R.id.edtProfEditAddress);
+        Button btnSave = (Button) dialog.findViewById(R.id.btnProfEditSave);
+        Button btnCancel = (Button) dialog.findViewById(R.id.btnProfEditCancel);
+        dialog.show();
+
+        // Save Profile
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        // Cancel
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private String encodeImage(Bitmap bm){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 100 , baos);
+        byte[] b = baos.toByteArray();
+        String encImage = Base64.encodeToString(b, Base64.DEFAULT);
+        return encImage;
     }
 
 }
